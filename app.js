547f74725db85c3aba3b8996223b9eb4d349467e
Yuvaraj Kumar <yuvisasi333@gmail.com>
2026-06-24 12:13:29 +0530

    First Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD Pipeline Working good");
+    res.send("CI/CD Pipeline Working!");
 });
 
 app.get("/health", (req, res) => {
