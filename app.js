8204931010818256802bb19793fbdb54982cb60f
Yuvaraj Kumar <yuvisasi333@gmail.com>
2026-06-24 15:55:52 +0530

    Committed Successfully

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD is working good");
+    res.send("CI/CD is Fine!");
 });
 
 app.get("/health", (req, res) => {
