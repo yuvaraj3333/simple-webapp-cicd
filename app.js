6dbff33a9f1f4396c0e2bfb6c7be99def555d4d5
Yuvaraj Kumar <yuvisasi333@gmail.com>
2026-06-24 11:09:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("CI/CD Pipeline Working!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
