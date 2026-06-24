7768535c69c54efc5d876ac78c2cac76c696a771
Yuvaraj Kumar <yuvisasi333@gmail.com>
2026-06-24 12:25:51 +0530

    Third Commit done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD is working nice!");
+    res.send("CI/CD is working good");
 });
 
 app.get("/health", (req, res) => {
