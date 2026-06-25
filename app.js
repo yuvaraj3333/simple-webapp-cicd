5aeb636669920162ddc1bd68ba9b3674dd09b958
Yuvaraj Kumar <yuvisasi333@gmail.com>
2026-06-25 12:21:46 +0530

    Committed Successfully

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>DevOps Monitoring Dashboard</title>
+<title>DevOps Monitoring Dashboard View</title>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
